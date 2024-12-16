885bdd3ee43e09627de2be692dcb8856dd6f4d40
Ignacio Arroyo <axayakac@gmail.com>
2024-12-16 12:46:49 -0600

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def main():
         url = "http://192.168.241.210:9200"
         index = 'triplets'
         es_page_size = 100
-        max_docs2load = 2000
+        max_docs2load = 1500
 
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         logger.info(f"Device found: {device}")
