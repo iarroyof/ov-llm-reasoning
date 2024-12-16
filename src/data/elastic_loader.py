6ef42a23136b2fc5967a803b126cd138dccad8c6
Ignacio Arroyo <axayakac@gmail.com>
2024-12-16 12:18:06 -0600

    Update elastic_loader.py

--- src/data/elastic_loader.py
+++ src/data/elastic_loader.py
@@ -86,7 +86,7 @@ class ElasticSearchDataset(IterableDataset):
         Returns the number of batches in the dataset.
         For elastic search, this is max_documents divided by batch_size, rounded up.
         """
-        return (self.max_documents + self.batch_size - 1) // self.batch_size
+        return (len(self.document_ids) + self.batch_size - 1) // self.batch_size
 
     def _get_all_document_ids(self, max_documents: int) -> List[str]:
         search_query = {
