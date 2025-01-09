0a4383ee46b0a880afe93b0772b8b6044c799558
Ignacio Arroyo <axayakac@gmail.com>
2025-01-08 18:56:58 -0600

    Update __init__.py
    Added XXL T5 model

--- src/trainers/__init__.py
+++ src/trainers/__init__.py
@@ -1,11 +1,13 @@
 from .trainer_base import BaseNeuralReasoningTrainer
 from .trainer_t5 import T5ReasoningTrainer
+from .trainer_t5_xxl import T5LargeReasoningTrainer
 from .trainer_bart import BartReasoningTrainer
 from .trainer_pegasus import PegasusReasoningTrainer
 
 __all__ = [
     'BaseNeuralReasoningTrainer',
     'T5ReasoningTrainer',
+    'T5LargeReasoningTrainer',
     'BartReasoningTrainer',
     'PegasusReasoningTrainer'
 ]
