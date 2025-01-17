63c4be26b84da271eefb78e67c7eafb7667012c6
Ignacio Arroyo <axayakac@gmail.com>
2025-01-17 17:39:50 -0600

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,13 +1,13 @@
 from .cache import ClearCache
-from .triplet_utils import FilterMethod
-from .triplet_utils import TripletFilter
 from .cache_utils import save_split_cache
 from .cache_utils import load_split_cache
 from .es_settings import es_settings
+from .triplet_filter import FilterMethod, TripletFilter, process_and_filter_triplets
 
 __all__ = ['ClearCache']
-__all__ = ['FilterMethod']
-__all__ = ['TripletFilter']
-__all__ = ['es_settings']
 __all__ = ['save_split_cache']
 __all__ = ['load_split_cache']
+__all__ = ['es_settings']
+__all__ = ['FilterMethod']
+__all__ = ['TripletFilter']
+__all__ = ['process_and_filter_triplets']
