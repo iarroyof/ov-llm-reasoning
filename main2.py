ddaa9db80cc386dfc51cf962f6c4cdfcc8f6a725
Blacvas <ulisesq85@gmail.com>
2025-06-26 13:15:57 -0600

    Correccion tokenizer por processing_class

--- main2.py
+++ main2.py
@@ -154,7 +154,7 @@ trainer = Seq2SeqTrainer(
     args=training_args,
     train_dataset=train_dataset,
     eval_dataset=test_dataset,
-    tokenizer=tokenizer,
+    processing_class=tokenizer,
     data_collator=data_collator,
     compute_metrics=compute_metrics,
 )
