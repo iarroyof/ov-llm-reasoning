3cb808311c04eec4f0468667477af56dbccaebbd
Blacvas <ulisesq85@gmail.com>
2025-06-30 12:24:07 -0600

    Impresion de entradas

--- main3.py
+++ main3.py
@@ -86,7 +86,9 @@ def prepare_data2(line: str,
     """
     # Asume que las columnas están separadas por comas y en este orden:
     # 0: article_id, 1: sentence_text, 2: subject, 3: relation, 4: object
+    print("Antes del split por comas\n", line)
     cols = line.strip().split(",")
+    print("Despues del split por comas\n", cols)
 
     subject = cols[2]
     relation = cols[3]
