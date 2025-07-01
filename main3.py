42c181566cea1fc57f962d397f53b0e10a6a827d
Blacvas <ulisesq85@gmail.com>
2025-07-01 13:20:35 -0600

    Colocando adafactor

--- main3.py
+++ main3.py
@@ -193,6 +193,8 @@ def main():
         report_to=["wandb"],
         load_best_model_at_end=True,
         metric_for_best_model="eval_loss",
+        adafactor = True
+        #optim = "adafactor"
     )
 
     trainer = Trainer(model=model,
