c6cef3e8e8217fc5509baee0640510d41630e0be
Blacvas <ulisesq85@gmail.com>
2025-08-06 13:30:16 -0600

    Imprimiendo tipo de dato en bertscores

--- main3.py
+++ main3.py
@@ -250,6 +250,8 @@ def main():
             pd.DataFrame({"Subj_Pred": hold_inp, "Obj": hold_preds, "Obj_true": hold_tgt}).to_csv(
                 os.path.join(out_dir, "test_predictions.tsv"), sep="\t", index=False)
             results = bertscore.compute(predictions=hold_preds, references=hold_tgt, lang="en")
+            print("Tipo de dato hold pred", type(hold_preds))
+            print("Tipo de datk, hold trg", type(hold_tgt))
             print("Bert_Score holdoutdata: ", results)
 
     if cfg.holdoutData and os.path.exists(cfg.holdoutData):
