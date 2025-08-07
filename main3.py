3562963e18e5a7c983d97c490fd0327be9291c0b
Blacvas <ulisesq85@gmail.com>
2025-08-06 18:57:18 -0600

    Agregando metrica rouge

--- main3.py
+++ main3.py
@@ -245,6 +245,12 @@ def main():
     pd.DataFrame({"Subj_Pred": val_inp, "Obj": val_preds, "Obj_true": val_tgt}).to_csv(
         os.path.join(out_dir, "predictions.tsv"), sep="\t", index=False)
 
+    rouge = Rouge()
+
+    rouge1_scores = []
+    rouge2_scores = []
+    rougeL_scores = []
+
     # Hold‑out predictions
     if cfg.holdoutData and os.path.exists(cfg.holdoutData):
         #with open(cfg.holdoutData) as f: hold_lines = f.readlines()
@@ -261,6 +267,11 @@ def main():
             print(f"Bert_Score Precision: {Bert_Pres.mean().item():.4f}")
             print(f"Bert_Score Recall: {Bert_Recall.mean().item():.4f}")
             print(f"Bert_Score F1Score: {Bert_F1.mean().item():.4f}")
+
+            scores = rouge.get_scores(hold_preds, list(hold_tgt))
+            print(f"rouge-1 : {scores['rouge-1']['f'].mean().item():.4f}")
+            print(f"rouge-2 : {scores['rouge-2']['f'].mean().item():.4f}")
+            print(f"rouge-l : {scores['rouge-l']['f'].mean().item():.4f}")
 
     if cfg.holdoutData and os.path.exists(cfg.holdoutData):
         print("="*100)
