4a1a697f61e8eb7f93f8361c3fc56d9b26675504
Blacvas <ulisesq85@gmail.com>
2025-08-11 12:42:47 -0600

    Agregando un tercer modelo

--- main3.py
+++ main3.py
@@ -601,6 +601,6 @@ def prueba_tripletas(file_path, model, tokenizer, device, chunk_size):
     print(f"ROUGE-L: {final_metrics['rougeL']:.4f}")
 
 if __name__ == "__main__":
-    models = ['t5-base',"Kevincp560/t5-base-finetuned-pubmed"]
+    models = ['t5-base',"Kevincp560/t5-base-finetuned-pubmed", 'gayanin/t5-small-finetuned-pubmed']
     for modelname in models:
         main(modelname)
