0796d44567c64b71eee398e3bac86c0e6e5f9632
Blacvas <ulisesq85@gmail.com>
2025-08-11 11:13:43 -0600

    Imprimiendo data

--- main4.py
+++ main4.py
@@ -4,5 +4,5 @@ import pandas as pd
 df_train = pd.read_csv("data/filtered_train_triplets_shuffle.csv")
 df_test = pd.read_csv("data/filtered_test_triplets_shuffle.csv")
 
-df_train.head()
-df_test.head()
+print(df_train.head())
+print(df_test.head())
