d630de5cf3904613cc63e4ac1a711b211bb61195
Blacvas <ulisesq85@gmail.com>
2025-08-11 11:12:05 -0600

    Agregando script main4 para la lectura de los tripletas filtradas en data

--- main4.py
+++ main4.py
@@ -1,8 +1,8 @@
 # En este codigo se explorar el ajuste fino sin que se guarde en wandb y verificar los resultados de entrenamiento
 import pandas as pd
 
-df_train = pd.read_csv("filtered_train_triplets_shuffle.csv")
-df_test = pd.read_csv("filtered_test_triplets_shuffle.csv")
+df_train = pd.read_csv("data/filtered_train_triplets_shuffle.csv")
+df_test = pd.read_csv("data/filtered_test_triplets_shuffle.csv")
 
 df_train.head()
 df_test.head()
