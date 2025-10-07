15eb3d5383ad09286768b0e695d425234967eeab
Blacvas <ulisesq85@gmail.com>
2025-10-07 13:38:01 -0600

    Correccion error al escribir conceptnet

--- main4.py
+++ main4.py
@@ -479,7 +479,7 @@ if __name__ == "__main__":
     dic_save_BERT_Scores = {}
     dic_save_Rouge_Scores = {}
     models = ["t5-base"] #'t5-base' #,"Kevincp560/t5-base-finetuned-pubmed", 'bleuLabs/t5-small-finetuned-pubmedSum'
-    datasets = ['concepnet']
+    datasets = ['conceptnet']
     for modelname in models:
         for dataset in datasets:
             dic_save_BERT_Scores[modelname], dic_save_Rouge_Scores[modelname], arguments = main(modelname, dataset)
