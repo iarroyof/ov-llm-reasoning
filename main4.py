775475cab30bb6986f62deeccc64b9f1220e4622
Blacvas <ulisesq85@gmail.com>
2025-10-16 10:27:50 -0600

    Aumentando el dataset de entrenamiento a 10000 para ambos dataset, concetpnet y biomedico

--- main4.py
+++ main4.py
@@ -92,8 +92,8 @@ def main(model_name, dataset):
     ap.add_argument("--description", required=True)
     ap.add_argument("--shuffle", default=False)              #Parametro que control el aleatorizado de las goldlabes para toma de metricas
     ap.add_argument("--save_f1score", default=False)        #Parametro que controla el exportado de los bertscores en formato tsv
-    ap.add_argument("--numTrainData_razon", default=10)
-    ap.add_argument("--numTrainData_biomed", default=10)
+    ap.add_argument("--numTrainData_razon", default=10000)
+    ap.add_argument("--numTrainData_biomed", default=10000)
     args = ap.parse_args()
 
     run = wandb.init(project="t5_spo_generation", config=vars(args))
