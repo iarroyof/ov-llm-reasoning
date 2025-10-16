e269348e111305a37deae02194b4aba9150487db
Blacvas <ulisesq85@gmail.com>
2025-10-16 10:21:27 -0600

    Cambiando la lista de parametros de entrenamiento y desabiulitando adafactor

--- main4.py
+++ main4.py
@@ -184,14 +184,21 @@ def main(model_name, dataset):
         num_train_epochs=cfg.nEpochs,
         per_device_train_batch_size=cfg.batchSize,
         per_device_eval_batch_size=cfg.batchSize,
+
+        learning_rate=5e-5,  # Se establece un learinig rate
+        weight_decay=0.01,   # Ayuda a prevenir el sobreajuste
+        lr_scheduler_type='linear', # Un scheduler lineal con calentamiento para un proceso efectivo
+        warmup_steps=500,    # Numero de pasos de calentamiento para estabilizar el inicio
+        optim="adamw_torch",
+
         evaluation_strategy="epoch",
         save_strategy="epoch",
         logging_strategy="epoch",
         report_to=["wandb"],
         load_best_model_at_end=True,
         metric_for_best_model="eval_loss",
-        adafactor = True,
-        optim = "adafactor"
+        #adafactor = True,
+        #optim = "adafactor"
     )
 
     trainer = Trainer(model=model,
