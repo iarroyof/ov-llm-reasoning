1f9ee234d531b466037406611a49c4416ee725d0
Blacvas <ulisesq85@gmail.com>
2025-12-15 15:10:57 -0600

    Se modifico para realizar experimentos guardando las metricas de validacion en wandb evaluando los modelos T5 small, base y bert large y base

--- main4.py
+++ main4.py
@@ -540,7 +540,7 @@ if __name__ == "__main__":
     dic_save_BERT_Scores = {}
     dic_save_Rouge_Scores = {}
     dic_save_Blue_Scores = {}
-    models = ["t5-small"] #facebook/bart-large #'t5-base' #facebook/bart-base #,"Kevincp560/t5-base-finetuned-pubmed", 'bleuLabs/t5-small-finetuned-pubmedSum'
+    models = ["t5-small", 't5-base', 'facebook/bart-large', 'facebook/bart-base'] #facebook/bart-large #'t5-base' #facebook/bart-base #,"Kevincp560/t5-base-finetuned-pubmed", 'bleuLabs/t5-small-finetuned-pubmedSum'
     datasets = ['conceptnet','SNLI']
     for modelname in models:
         for dataset in datasets:
