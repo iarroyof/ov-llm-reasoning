6a4c96550877bc33dca48f54e81accb1534f929b
Blacvas <ulisesq85@gmail.com>
2026-01-21 18:15:42 -0600

    Cambiando a modelo t5 base

--- main4.py
+++ main4.py
@@ -562,7 +562,7 @@ if __name__ == "__main__":
     dic_save_BERT_Scores = {}
     dic_save_Rouge_Scores = {}
     dic_save_Blue_Scores = {}
-    models = ["t5-small"] # "t5-small", 't5-base', 't5-large' #'facebook/bart-large' #'facebook/bart-base' #,"Kevincp560/t5-base-finetuned-pubmed", 'bleuLabs/t5-small-finetuned-pubmedSum'
+    models = ["t5-base"] # "t5-small", 't5-base', 't5-large' #'facebook/bart-large' #'facebook/bart-base' #,"Kevincp560/t5-base-finetuned-pubmed", 'bleuLabs/t5-small-finetuned-pubmedSum'
     datasets = ['atomic','conceptnet','SNLI'] #'conceptnet','SNLI'
     for modelname in models:
         for dataset in datasets:
