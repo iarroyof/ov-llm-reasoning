8782bd2a57a9b619582c1c73f6f5899d302b9ecb
Blacvas <ulisesq85@gmail.com>
2026-01-29 09:18:35 -0600

    Camibando dataset a atomic

--- main4.py
+++ main4.py
@@ -564,7 +564,7 @@ if __name__ == "__main__":
     dic_save_Rouge_Scores = {}
     dic_save_Blue_Scores = {}
     models = ["t5-large"] # "t5-small", 't5-base', 't5-large' #'facebook/bart-large' #'facebook/bart-base' #,"Kevincp560/t5-base-finetuned-pubmed", 'bleuLabs/t5-small-finetuned-pubmedSum'
-    datasets = ['SNLI'] #'conceptnet','SNLI', atomic
+    datasets = ['atomic'] #'conceptnet','SNLI', atomic
     for modelname in models:
         for dataset in datasets:
             # Limpieza ANTES de cada experimento
