a8ec88d5c9bdb976a451875d03150f4d0be2b9a4
Blacvas <ulisesq85@gmail.com>
2026-02-02 18:53:37 -0600

    Cambiando de modelo bart base a large con el dtatset de snli

--- main4.py
+++ main4.py
@@ -563,7 +563,7 @@ if __name__ == "__main__":
     dic_save_BERT_Scores = {}
     dic_save_Rouge_Scores = {}
     dic_save_Blue_Scores = {}
-    models = ["facebook/bart-lare"] # "t5-small", 't5-base', 't5-large' #'facebook/bart-large' #'facebook/bart-base' #,"Kevincp560/t5-base-finetuned-pubmed", 'bleuLabs/t5-small-finetuned-pubmedSum'
+    models = ["facebook/bart-large"] # "t5-small", 't5-base', 't5-large' #'facebook/bart-large' #'facebook/bart-base' #,"Kevincp560/t5-base-finetuned-pubmed", 'bleuLabs/t5-small-finetuned-pubmedSum'
     datasets = ['SNLI'] #'conceptnet','SNLI', 'atomic'
     for modelname in models:
         for dataset in datasets:
